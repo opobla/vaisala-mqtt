637bf5082feea431f0cf3c84b3cb751fb3ce4fe1
Óscar García <oscarpoblacion@gmail.com>
2024-02-02 21:01:44 +0000

    Add exponential backoff on telnet failure

--- drivers.py
+++ drivers.py
@@ -1,6 +1,52 @@
-import re
 from datetime import datetime, timedelta
+from functools import wraps
+import re
 import telnetlib
+import time
+
+def retry(ExceptionToCheck, tries=4, delay=3, backoff=2, logger=None):
+    """Retry calling the decorated function using an exponential backoff.
+
+    http://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/
+    original from: http://wiki.python.org/moin/PythonDecoratorLibrary#Retry
+
+    :param ExceptionToCheck: the exception to check. may be a tuple of
+        exceptions to check
+    :type ExceptionToCheck: Exception or tuple
+    :param tries: number of times to try (not retry) before giving up
+    :type tries: int
+    :param delay: initial delay between retries in seconds
+    :type delay: int
+    :param backoff: backoff multiplier e.g. value of 2 will double the delay
+        each retry
+    :type backoff: int
+    :param logger: logger to use. If None, print
+    :type logger: logging.Logger instance
+    """
+    def deco_retry(f):
+
+        @wraps(f)
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay
+            while mtries > 1:
+                try:
+                    return f(*args, **kwargs)
+                except ExceptionToCheck:
+                    msg = "%s, Retrying in %d seconds..." % (str(ExceptionToCheck), mdelay)
+                    if logger:
+                        logger.exception(msg) # would print stack trace
+                        logger.warning(msg)
+                    else:
+                        print(msg)
+                    time.sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= backoff
+            return f(*args, **kwargs)
+
+        return f_retry  # true decorator
+
+    return deco_retry
+
 
 class VaisalaDriver:
     """The vaisala driver
@@ -26,14 +72,20 @@ class VaisalaDriver:
             self.telnet_client.write(self.SEND_CMD_OUTPUT_FORMAT.encode('ASCII'))
             self.telnet_client.read_until(">".encode("ascii"))
         return self._telnet_client 
-            
+
+    @retry((TimeoutError, Exception), tries=200, delay=1, backoff=1.1)
     def update_info(self):
-        self.telnet_client.write("SEND".encode("ascii") + b"\r")
-        self.telnet_client.read_until(b"\n")
-        response = self.telnet_client.read_until(b"\n")
-        self.pressure_in_hPa, self.temperature_in_celsius, self.relative_humidity = self.parse_response(response)
-        self.last_update = datetime.now()
-        return self.pressure_in_hPa, self.temperature_in_celsius, self.relative_humidity
+        try:
+            self.telnet_client.write("SEND".encode("ascii") + b"\r")
+            self.telnet_client.read_until(b"\n")
+            response = self.telnet_client.read_until(b"\n")
+            self.pressure_in_hPa, self.temperature_in_celsius, self.relative_humidity = self.parse_response(response)
+            self.last_update = datetime.now()
+            return self.pressure_in_hPa, self.temperature_in_celsius, self.relative_humidity
+        except Exception as e:
+            # Force a new telnet connetion to be made in the future
+            self._telnet_client = None
+            raise
 
     def get_pressure(self):
         if self.is_info_expired(): self.update_info()
